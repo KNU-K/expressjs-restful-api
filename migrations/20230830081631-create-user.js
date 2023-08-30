d424b16fc4b725f3906175cb6eeca228682970f0
KNU-K <knukang334@gmail.com>
2023-08-30 18:49:26 +0900

    passport 구성
    로그인 회원가입 기능 구성
    serialize deserialize 적용
    express-session 사용

---
+++ migrations/20230830081631-create-user.js
@@ -0,0 +1,36 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Users", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      userid: {
+        type: Sequelize.STRING,
+      },
+      userpw: {
+        type: Sequelize.STRING,
+      },
+      username: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Users");
+  },
+};
--- server.js
+++ server.js
@@ -1,10 +1,28 @@
 const express = require("express");
 const app = express();
 const routers = require("./routers");
+<<<<<<< Updated upstream
+=======
+const db = require("./models");
+const session = require("express-session");
+const { strategyMiddleware } = require("./middlewares/strategy");
+const passport = require("passport");
+
+>>>>>>> Stashed changes
 const port = 3333;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false },
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 app.use("/api", routers);
 
 app.listen(port, () => {
