8cc8b1da5cc037083555dbba07b6f2835e36d714
KNU-K <knukang334@gmail.com>
2023-08-30 17:23:02 +0900

    server 파일에서 sequelize 테스트 진행
    db.sequelize.sync() 함수를 통해 sequelize를 통한 db가 제대로 동작되는지 확인하는 동작을 넣었다.(비동기 진행)

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const app = express();
 const routers = require("./routers");
+const db = require("./models");
 const port = 3333;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/api", routers);
 
-app.listen(port, () => {
-  console.log(`Server open port : ${port}`);
+app.listen(port, async () => {
+  try {
+    await db.sequelize.sync();
+    console.log("db conn ..!");
+
+    console.log(`finally, Server open port : ${port}`);
+  } catch (err) {
+    throw err;
+  }
 });
