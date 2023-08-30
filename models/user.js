a6207c16eaaab1ee76b75e5c1d814c63e00daac6
KNU-K <knukang334@gmail.com>
2023-08-30 17:21:11 +0900

    sequelize 를 통한 orm 구성 및 User model 생성
    로그인의 선수 작업인 db 구성을 통해 orm 을 통한 db 를 구성하고 로그인을 위한 User model 을 생성하였다.
    
    app.listen이 되기 이전에 db.sequelize를 통해 db 연결을 테스트 하였다.

---
+++ models/user.js
@@ -0,0 +1,36 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init(
+    {
+      userid: {
+        type: DataTypes.STRING,
+        primaryKey: true,
+        allowNull: false,
+      },
+      userpw: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: "User",
+    }
+  );
+  return User;
+};
